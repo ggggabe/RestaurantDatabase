371f50b9848a3dcf344ad7ae3a0613646877e80a
amarquez727 <amarquez727@gmail.com>
2014-11-11 19:34:02 -0500

    added just restaurant names csv

--- generate.py
+++ generate.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
 import pandas as pd
 
 restaurants = pd.read_csv("Restaurants.csv")
-for name in restaurants.Restaurant :
-    print name 
+
+with open("restaurant_names.csv", 'w+') as rn :
+    for name in restaurants.Restaurant :
+        print(name, file=rn)
